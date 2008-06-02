a3c16cec273eea168258be94749f498d5e74dd28
Trocotronic <trocotronic@redyc.com>
2008-06-02 18:59:31 +0000

    020508253 - Entrada para el synch del SVN.
    git-svn-id: file:///Users/trocotronic/Sites/redyc/svn-redyc/UDB/trunk@948 5d1adacc-3b79-6a49-8c4e-044fc10dbfbf

---
+++ trunk/include/version.h
@@ -0,0 +1,68 @@
+/*
+**
+** version.h
+** UnrealIRCd
+** $Id: version.h,v 1.1.1.1.2.48 2008/04/09 13:41:05 Trocotronic Exp $
+*/
+#ifndef __versioninclude
+#define __versioninclude 1
+
+/* 
+ * Mark of settings
+ */
+#ifdef DEBUGMODE
+#define DEBUGMODESET "+(debug)"
+#else
+#define DEBUGMODESET ""
+#endif
+ /**/
+#ifdef DEBUG
+#define DEBUGSET "(Debug)"
+#else
+#define DEBUGSET ""
+#endif
+     /**/
+#define COMPILEINFO DEBUGMODESET DEBUGSET
+
+/* Version info follows
+ * Please be sure to update ALL fields when changing the version.
+ * Also don't forget to bump the protocol version every release.
+ */
+
+/** These UNREAL_VERSION_* macros can be used so (3rd party) modules
+ * can easily distinguish versions.
+ */
+
+/** The generation version number (eg: 3 for Unreal3*) */
+#define UNREAL_VERSION_GENERATION   3
+
+/** The major version number (eg: 2 for Unreal3.2*) */
+#define UNREAL_VERSION_MAJOR        2
+
+/** The minor version number (eg: 1 for Unreal3.2.1), negative numbers for unstable/alpha/beta */
+#define UNREAL_VERSION_MINOR        7
+
+/** Year + week of the year (with Monday as first day of the week).
+ * Can be useful if the above 3 versionids are insufficient for you (eg: you want to support CVS).
+ * This is updated automatically on the CVS server every Monday. so don't touch it.
+ */
+#define UNREAL_VERSION_TIME         200809
+
+#define UnrealProtocol 		2309
+#define PATCH1  		"3"
+#define PATCH2  		".2"
+#define PATCH3  		".7"
+#ifdef UDB
+#define PATCH4 			"+UDB-3.6es"
+#else
+#define PATCH4  		""
+#endif
+#define PATCH5  		""
+#define PATCH6  		""
+#define PATCH7  		""
+#define PATCH8  		COMPILEINFO
+#define PATCH9  		""
+/* release header */
+#define Rh BASE_VERSION
+#define VERSIONONLY		PATCH1 PATCH2 PATCH3 PATCH4 PATCH5 PATCH6 PATCH7
+#endif /* __versioninclude */
